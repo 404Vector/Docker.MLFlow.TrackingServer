96053099e1a1a460169ec9a6e27f3f2832762e84
404Vector-mac <tiryul@gmail.com>
2022-11-13 21:58:47 +0900

    update train.py: add logging code

--- example_project/train.py
+++ example_project/train.py
@@ -7,10 +7,10 @@ import numpy as np
 from sklearn.linear_model import LogisticRegression
 
 import mlflow
-import mlflow.sklearn
+
+from mlflow import log_metric, log_param, log_artifacts
 
 if __name__ == "__main__":
-    mlflow.sklearn.autolog()
     X = np.array([-2, -1, 0, 1, 2, 1]).reshape(-1, 1)
     y = np.array([0, 0, 1, 1, 1, 0])
 
@@ -18,7 +18,12 @@ if __name__ == "__main__":
 
     with mlflow.start_run() as run:
         lr.fit(X, y)
-        
-    score = lr.score(X, y)
+
+    log_param("solver", sys.argv[1])
+    log_param("penalty", sys.argv[2])
+    log_param("l1_ratio", sys.argv[3])
+
+    score = float(lr.score(X, y))
+    log_metric("score", score)
     print("Score: %s" % score)
    
